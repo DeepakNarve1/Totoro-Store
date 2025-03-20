d5457ed68ae52eda114f819da0df191464da343e
Deepaknarve <deepaknarve4@gmail.com>
2025-03-20 16:27:21 +0530

    Add footer

--- frontend/src/components/Common/Navbar.jsx
+++ frontend/src/components/Common/Navbar.jsx
@@ -4,14 +4,19 @@ import { useState } from "react";
 import {
   HiOutlineUser,
   HiOutlineShoppingBag,
-  HiBars3,
   HiBars3BottomRight,
 } from "react-icons/hi2";
 import SearchBar from "./SearchBar";
 import CartDrawer from "../Layout/CartDrawer";
+import { IoMdClose } from "react-icons/io";
 
 const Navbar = () => {
   const [drawerOpen, setDrawerOpen] = useState(false);
+  const [navDrawerOpen, setNavDrawerOpen] = useState(false);
+
+  const toggleNavDrawer = () => {
+    setNavDrawerOpen(!navDrawerOpen);
+  };
 
   const toggleCartDrawer = () => {
     setDrawerOpen(!drawerOpen);
@@ -72,12 +77,57 @@ const Navbar = () => {
             <SearchBar />
           </div>
 
-          <button className="md:hidden">
+          <button onClick={toggleNavDrawer} className="md:hidden">
             <HiBars3BottomRight className="h-6 w-6 text-gray-800" />
           </button>
         </div>
       </nav>
       <CartDrawer drawerOpen={drawerOpen} toggleCartDrawer={toggleCartDrawer} />
+
+      {/* Mobile Navigation */}
+      <div
+        className={`fixed top-0 left-0 w-3/4 sm:w-1/2 md:w-1/3 h-full bg-white shadow-lg transform transition-transform duration-300 z-50 ${
+          navDrawerOpen ? "translate-x-0" : "-translate-x-full"
+        }`}
+      >
+        <div className="flex justify-end p-4">
+          <button onClick={toggleNavDrawer}>
+            <IoMdClose className="h-6 w-6 text-gray-800" />
+          </button>
+        </div>
+        <div className="p-4">
+          <h2 className="text-xl font-semibold mb-4">Menu</h2>
+          <nav className="space-y-4">
+            <Link
+              to="#"
+              onClick={toggleNavDrawer}
+              className="block text-gray-800 hover:text-gray-600"
+            >
+              Men
+            </Link>
+            <Link
+              to="#"
+              onClick={toggleNavDrawer}
+              className="block text-gray-800 hover:text-gray-600"
+            >
+              Women
+            </Link><Link
+              to="#"
+              onClick={toggleNavDrawer}
+              className="block text-gray-800 hover:text-gray-600"
+            >
+              Top Wear
+            </Link>
+            <Link
+              to="#"
+              onClick={toggleNavDrawer}
+              className="block text-gray-800 hover:text-gray-600"
+            >
+              Bottom Wear
+            </Link>
+          </nav>
+        </div>
+      </div>
     </>
   );
 };
