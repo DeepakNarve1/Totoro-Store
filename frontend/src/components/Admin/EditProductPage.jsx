cc3694ce9179ff780b732f8a9268b9a2fe632795
Deepaknarve <deepaknarve8989@gmail.com>
2025-03-30 20:43:24 +0530

    Admin section Layout

---
+++ frontend/src/components/Admin/EditProductPage.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+const EditProductPage = () => {
+  const [productData, setProductData] = useState({
+    name: "",
+    description: "",
+    price: 0,
+    countInStock: 0,
+  });
+  return <div>EditProductPage</div>;
+};
+
+export default EditProductPage;
